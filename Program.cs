05b34a03ca0485927b40f8293a03e98aad36923f
erenenes <41615432+erenenes@users.noreply.github.com>
2021-07-08 09:57:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+namespace Pisagor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3-4-5 // 5-12-13 // 8-15-17 // 7-24-25
+            int n = 1000;
+            int[] trianglesTotal = new int[] { 12, 30, 40, 56 };
+            int[,] triangles = new int[,] { { 3, 4, 5 }, { 5, 12, 13 }, { 8, 15, 17 }, { 7, 24, 25 } };
+            int multiple = 1;
+            int index = 0;
+            int[] result = new int[3];
+
+            for (int i = 0; i < trianglesTotal.Length; i++)
+            {
+                if (n % trianglesTotal[i] == 0)
+                {
+                    multiple = n / trianglesTotal[i];
+                    index = i;
+                    break;
+                }
+            }
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = triangles[index, i] * multiple;
+            }
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result[1]);
+            Console.WriteLine(result[2]);
+            Console.ReadKey();
+        }
+    }
+}
